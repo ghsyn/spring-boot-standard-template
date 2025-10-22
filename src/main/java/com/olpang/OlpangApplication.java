80c80fe948090df6c4bfbe48559cdae6a43c963e
ghsyn <swanshawn715@gmail.com>
2025-10-22 17:15:11 +0900

    init: 프로젝트 생성
    Spring Boot 버전: 3.5.6.

---
+++ src/main/java/com/olpang/OlpangApplication.java
@@ -0,0 +1,13 @@
+package com.olpang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlpangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OlpangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/olpang/OlpangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olpang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlpangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
