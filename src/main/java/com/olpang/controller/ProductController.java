e0c936779876571e54d8355a28ddd7e4bd6e9433
ghsyn <swanshawn715@gmail.com>
2025-10-22 21:20:46 +0900

    feat: ProductController POST API 생성
    제품 등록 엔드포인트 생성,
    요청 값 JSON에 담아 POST /api/v1/products 요청 시 "제품 등록" 문자열 반환.

--- src/main/java/com/olpang/controller/ProductController.java
+++ src/main/java/com/olpang/controller/ProductController.java
@@ -1,12 +1,27 @@
 package com.olpang.controller;
 
+import com.olpang.request.ProductCreateRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class ProductController {
+
+    /**
+     * 제품 등록
+     * @return '제품 등록'
+     */
+    @PostMapping("/api/v1/products")
+    public String post(@RequestBody ProductCreateRequest request) {
+        log.info("request = {}", request.toString());
+        return "제품 등록";
+    }
 
     /**
      * 제품 목록 조회
