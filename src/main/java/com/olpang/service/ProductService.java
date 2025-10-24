8440104eccc2e47cb04a385a3adceffd619945c9
ghsyn <swanshawn715@gmail.com>
2025-10-24 20:38:34 +0900

    feat: ProductService 제품 단건 조회 로직 구현
    제품 단건 조회 서비스 구현,
     - 받아온 id에 해당하는 제품의 상세 정보를 응답 dto 형태로 반환
     - 찾는 제품이 없을 시 발생하는 exception 개선 필요
    테스트(ProductServiceTest.getDetailsTest) 통과 확인.

--- src/main/java/com/olpang/service/ProductService.java
+++ src/main/java/com/olpang/service/ProductService.java
@@ -3,6 +3,7 @@ package com.olpang.service;
 import com.olpang.domain.Product;
 import com.olpang.repository.ProductRepository;
 import com.olpang.request.ProductCreateRequest;
+import com.olpang.response.ProductDetailsResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -22,5 +23,19 @@ public class ProductService {
                 .build();
 
         productRepository.save(product);
+    }
+
+    public ProductDetailsResponse getDetails(Long id) {
+        Product product = productRepository.findById(id)
+                // .orElse( ... ) TODO: ID 값 없다면 제품 등록 전이라는 적절한 exception 만들기
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 제품입니다."));
+
+        // "WebProductService: 호출 담당 서비스 클래스" / "ProductService: 외부 통신 및 데이터 처리 담당 서비스 클래스"로 나누어 확장 가능
+        return ProductDetailsResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .brand(product.getBrand())
+                .description(product.getDescription())
+                .build();
     }
 }
