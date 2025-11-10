c40550d447fe896c70904972841f96a20982e066
ghsyn <swanshawn715@gmail.com>
2025-11-10 20:59:37 +0900

    feat: ControllerAdvice 내 ProductNotFoundException 핸들러 추가
    ProductNotFoundException 대한 표준 예외 처리 핸들러 추가,
    테스트(ProductControllerTest.getDetailsNotFoundTest, editProductNotFoundTest, deleteProductNotFoundTest) 통과 확인.

--- src/main/java/com/olpang/advice/CommonControllerAdvice.java
+++ src/main/java/com/olpang/advice/CommonControllerAdvice.java
@@ -1,5 +1,6 @@
 package com.olpang.advice;
 
+import com.olpang.exception.ProductNotFoundException;
 import com.olpang.response.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
@@ -40,6 +41,28 @@ public class CommonControllerAdvice {
         for (FieldError fieldError : e.getFieldErrors()) {
             response.addValidation(fieldError.getField(), fieldError.getDefaultMessage());
         }
+
+        return response;
+    }
+
+    /**
+     * {@link ProductNotFoundException} 예외 처리 핸들러.
+     *
+     * 존재하지 않는 제품 접근 시 발생하는 예외를 처리하여
+     * HTTP 404 응답과 함께 표준화된 에러 응답 객체를 반환
+     *
+     * @param e 발생한 ProductNotFoundException 예외
+     * @return ErrorResponse 객체
+     */
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(ProductNotFoundException.class)
+    @ResponseBody
+    public ErrorResponse productNotFoundHandler(ProductNotFoundException e) {
+
+        ErrorResponse response = ErrorResponse.builder()
+                .code("404")
+                .message(e.getMessage())
+                .build();
 
         return response;
     }
