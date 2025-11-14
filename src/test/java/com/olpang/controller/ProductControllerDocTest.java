28570ea675d219af4f2becddbacd12bab5b3a889
ghsyn <swanshawn715@gmail.com>
2025-11-14 16:44:47 +0900

    feat: Spring REST Docs 환경 구성, API 문서화 초기 구성 추가
    Spring REST Docs 도입 및 Asciidoctor Gradle 설정,
    ProductControllerDocTest 생성 및 빌드 폴더 내 제품 단건 조회 API 스니펫 생성,
    index.adoc 문서 생성 및 bootJar 빌드 시 index.html 파일 static 폴더로 자동 복사.

---
+++ src/test/java/com/olpang/controller/ProductControllerDocTest.java
@@ -0,0 +1,59 @@
+package com.olpang.controller;
+
+import com.olpang.domain.Product;
+import com.olpang.repository.ProductRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.http.MediaType.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@ExtendWith(RestDocumentationExtension.class)
+public class ProductControllerDocTest {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @BeforeEach
+    void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(documentationConfiguration(restDocumentation))
+                .build();
+    }
+
+    @Test
+    @DisplayName("제품 단건 조회 문서 테스트")
+    void test1() throws Exception {
+        // given
+        Product product = Product.builder()
+                .name("제품명")
+                .brand("제조사")
+                .description("제품설명")
+                .build();
+
+        productRepository.save(product);
+
+        // expected
+        mockMvc.perform(get("/api/v1/products/{productId}", 1L)
+                        .accept(APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("index"));
+    }
+}
