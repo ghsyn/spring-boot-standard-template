e7bc74e0a97bd14758c5f4b66d0a365652377b5c
ghsyn <swanshawn715@gmail.com>
2025-11-14 18:23:19 +0900

    feat: 제품 등록 API 문서 추가
    ProductControllerDocTest 제품 등록 테스트 추가 및 API 별 폴더 분리,
    제품 등록 API 스니펫 생성 및 index.adoc 문서 적용,
    Host명 변경, 요청 파리미터 및 응답 필드 설명 추가.

--- src/test/java/com/olpang/controller/ProductControllerDocTest.java
+++ src/test/java/com/olpang/controller/ProductControllerDocTest.java
@@ -1,50 +1,52 @@
 package com.olpang.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.olpang.domain.Product;
 import com.olpang.repository.ProductRepository;
-import org.junit.jupiter.api.BeforeEach;
+import com.olpang.request.ProductCreateRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.payload.PayloadDocumentation;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.http.MediaType.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs(uriScheme = "https", uriHost = "api.olpang.com", uriPort = 443)
 @ExtendWith(RestDocumentationExtension.class)
 public class ProductControllerDocTest {
 
+    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private ProductRepository productRepository;
 
-    @BeforeEach
-    void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-                .apply(documentationConfiguration(restDocumentation))
-                .build();
-    }
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
-    @DisplayName("제품 단건 조회 문서 테스트")
+    @DisplayName("제품 단건 조회")
     void test1() throws Exception {
         // given
         Product product = Product.builder()
-                .name("제품명")
-                .brand("제조사")
-                .description("제품설명")
+                .name("아토베리어365 크림")
+                .brand("에스트라")
+                .description("[11월 올영픽] 에스트라 아토베리어365 크림 80ml 더블 기획(+에센스 25ML + 세라-히알 앰플 7ML)")
                 .build();
 
         productRepository.save(product);
@@ -54,6 +56,44 @@ public class ProductControllerDocTest {
                         .accept(APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andDo(document("index"));
+                .andDo(document("product-inquiry",
+                        pathParameters(
+                            parameterWithName("productId").description("제품 ID")
+                        ),
+                        responseFields(
+                                fieldWithPath("id").description("제품 ID"),
+                                fieldWithPath("name").description("제품명"),
+                                fieldWithPath("brand").description("제조사"),
+                                fieldWithPath("description").description("제품 설명")
+                        )
+                ));
+    }
+
+    @Test
+    @DisplayName("제품 등록")
+    void test2() throws Exception {
+        // given
+        ProductCreateRequest request = ProductCreateRequest.builder()
+                .name("아토베리어365 크림")
+                .brand("에스트라")
+                .description("[11월 올영픽] 에스트라 아토베리어365 크림 80ml 더블 기획(+에센스 25ML + 세라-히알 앰플 7ML)")
+                .build();
+
+        String jsonRequest = objectMapper.writeValueAsString(request);
+
+        // expected
+        mockMvc.perform(post("/api/v1/products")
+                        .accept(APPLICATION_JSON)
+                        .contentType(APPLICATION_JSON)
+                        .content(jsonRequest))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("product-create",
+                        requestFields(
+                                PayloadDocumentation.fieldWithPath("name").description("제품명"),
+                                PayloadDocumentation.fieldWithPath("brand").description("제조사"),
+                                PayloadDocumentation.fieldWithPath("description").description("제품 설명")
+                        )
+                ));
     }
 }
